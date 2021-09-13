96d96226505bb080d8e67c9074b0cc646b6ad980
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-13 14:32:17 -0400

    updates to static HTML implementation

---
+++ Readme.md
@@ -0,0 +1,14 @@
+HTML Static Implementation
+
+Need 7 spaces for characters
+Need 7 images
+Need 26 buttons for A-Z letter options
+
+Functionality
+
+word list - https://raw.githubusercontent.com/suncoast-devs/handbook/master/assignments/assets/words.jsonreact-project-template
+
+When the player guesses a letter (clicks the button):
+All instances of that letter are revealed at their corresponding positions in the word.
+The button becomes disabled, as it has already been guessed.
+Display the snowman image that corresponds with the number of letters revealed in the word.
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
 import React from 'react'
+import step_0 from '..images/step_0.png'
 
 export function App() {
-  return <div>Hello, World</div>
+  return (
+    <main>
+      <header>
+        <h1>Build-A-Snowman</h1>
+      </header>
+      <body>
+        <section className="instructions">
+          <p>
+            <strong>Instructions:</strong> lorem ipsum
+          </p>
+        </section>
+        <section className="snowman-image">
+          <p>Placeholder</p>
+        </section>
+        <section className="word-display">_*_*_*_*_*_*_</section>
+        <section className="alphabet-buttons">
+          <button>A</button>
+          <button>B</button>
+          <button>C</button>
+          <button>D</button>
+          <button>E</button>
+          <button>F</button>
+          <button>G</button>
+          <button>H</button>
+          <button>I</button>
+          <button>J</button>
+          <button>K</button>
+          <button>L</button>
+          <button>M</button>
+          <button>N</button>
+          <button>O</button>
+          <button>P</button>
+          <button>Q</button>
+          <button>R</button>
+          <button>S</button>
+          <button>T</button>
+          <button>U</button>
+          <button>V</button>
+          <button>W</button>
+          <button>X</button>
+          <button>Y</button>
+          <button>Z</button>
+        </section>
+        <p>Test</p>
+      </body>
+      <footer>
+        <p>Built with ♥ in St. Petersburg, Florida.</p>
+      </footer>
+    </main>
+  )
 }
