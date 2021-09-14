7962c12e86306cf92d476e1cf0ee50f037fcfb55
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-13 20:08:24 -0400

    work on matching algorithm

--- Readme.md
+++ Readme.md
@@ -8,7 +8,7 @@ Functionality
 
 Add words list as words.json file - https://raw.githubusercontent.com/suncoast-devs/handbook/master/assignments/assets/words.jsonreact-project-template
 
-Randomize the word
+//Randomize the word
 
 When the player guesses a letter (clicks the button):
 check if the letter is in the string of the random word
@@ -17,3 +17,21 @@ display letter on random word line
 increase the image to the next in the sequence
 
 //The button becomes disabled, as it has already been guessed.
+
+Add a reset button that -
+
+resets snowman image
+clears guessed letters
+resets keyboard
+
+Algorithim for obtaining a random word and apply letter guess
+
+//Obtain the word as a string - const alphabetizeRandomWord takes getRandomWord to capture random word
+
+//parse word into new array of individual letters
+
+determine the index of the letters
+
+compare guessed letter to the array
+
+if match, use index to pair it with the display
--- src/App.tsx
+++ src/App.tsx
@@ -40,12 +40,28 @@ const alphabet = [
 ]
 
 export function App() {
+  //Randomized Word
   const getRandomWord = words[Math.floor(Math.random() * 1034)]
   console.log(getRandomWord)
-  const [guessedLetters, setGuessedLetters] = useState('')
+
+  //Split Random Word into array of letters
+  const alphabetizeRandomWord = getRandomWord.split('')
+  console.log(alphabetizeRandomWord)
+
+  //Compares user's letter guess against random word's array of letters
+  function matchRandomWord(letter: string) {
+    const result = alphabetizeRandomWord.includes(letter)
+
+    return result
+  }
+
+  const shawn = matchRandomWord
+  console.log(shawn)
+
+  const [guessedLetters, setGuessedLetters] = useState([''])
 
   function clickOnLetter(letter: string) {
-    const newValueForGuessedLetters = `${guessedLetters}${letter}`
+    const newValueForGuessedLetters = [...guessedLetters, letter]
     setGuessedLetters(newValueForGuessedLetters)
   }
 
@@ -54,64 +70,62 @@ export function App() {
       <header>
         <h1>Build-A-Snowman</h1>
       </header>
-      <main>
-        <section className="instructions">
-          <p>
-            <strong>Instructions:</strong> Select a letter to guess the random
-            word. Each correct letter builds another piece of your snowman. Have
-            fun!
-          </p>
-        </section>
-        <section className="snowman-image">
-          <img src={step7} alt="first step image of snow" />
-        </section>
-        <section className="word-display">
-          <h1>Word to Guess</h1>
-        </section>
-        <section className="random-word-display">
-          <h3 className="first-letter">
-            <u>*</u>
-          </h3>
-          <h3 className="second-letter">
-            <u>*</u>
-          </h3>
-          <h3 className="third-letter">
-            <u>*</u>
-          </h3>
-          <h3 className="fourth-letter">
-            <u>*</u>
-          </h3>
-          <h3 className="fifth-letter">
-            <u>*</u>
-          </h3>
-          <h3 className="sixth-letter">
-            <u>*</u>
-          </h3>
-          <h3 className="seventh-letter">
-            <u>*</u>
-          </h3>
-        </section>
-        <section className="letter-display">
-          Your guessed letters are: {guessedLetters}
-        </section>
-        <section className="alphabet-buttons">
-          {alphabet.map(function (letter) {
-            return (
-              <button
-                key={letter}
-                onClick={function () {
-                  clickOnLetter(letter)
-                }}
-                disabled={guessedLetters.includes(letter)}
-              >
-                {letter}
-              </button>
-            )
-          })}
-        </section>
 
-        {/* <div>Your guessed letters are {guessedLetters}</div> */}
-      </main>
+      <section className="instructions">
+        <p>
+          <strong>Instructions:</strong> Select a letter to guess the random
+          word. Each correct letter builds another piece of your snowman. Have
+          fun!
+        </p>
+      </section>
+      <section className="snowman-image">
+        <img src={step7} alt="first step image of snow" />
+      </section>
+      <section className="word-display">
+        <h1>Word to Guess</h1>
+      </section>
+      <section className="random-word-display">
+        <h3 className="0">
+          <u>*</u>
+        </h3>
+        <h3 className="1">
+          <u>*</u>
+        </h3>
+        <h3 className="2">
+          <u>*</u>
+        </h3>
+        <h3 className="3">
+          <u>*</u>
+        </h3>
+        <h3 className="4">
+          <u>*</u>
+        </h3>
+        <h3 className="5">
+          <u>*</u>
+        </h3>
+        <h3 className="6">
+          <u>*</u>
+        </h3>
+      </section>
+      <section className="letter-display">
+        <h5>Guessed letters: {guessedLetters}</h5>
+      </section>
+      <section className="alphabet-buttons">
+        {alphabet.map(function (letter) {
+          return (
+            <button
+              key={letter}
+              onClick={function () {
+                clickOnLetter(letter)
+              }}
+              disabled={guessedLetters.includes(letter)}
+            >
+              {letter}
+            </button>
+          )
+        })}
+      </section>
+
       <footer>
         <p>Built with ♥ in St. Petersburg, Florida.</p>
       </footer>
